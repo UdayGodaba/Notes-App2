aba12a8e464e34fbf06ba19ccab3e31df1aba413
UdayGodaba <godabauday@gmail.com>
2023-06-21 19:18:25 +0530

    Frontend api changes

--- frontend/src/network/Note_api.ts
+++ frontend/src/network/Note_api.ts
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV ? process.env.NODE_ENV : "local";
 const url = isProduction ? process.env.REACT_APP_PRODUCTION_URL : "http://localhost:5000";
 
 export const deleteNote = async (noteId: string) => {
-    await axios.delete(`${url}/api/notes` + noteId);
+    await axios.delete(`${url}/api/notes/` + noteId);
 };
 
 export interface NoteInput {
@@ -19,6 +19,6 @@ export const createNote = async (note: NoteInput): Promise<NoteModal> => {
 };
 
 export const updateNote = async (noteId: string, note: NoteInput): Promise<NoteModal> => {
-    const response = await axios.patch(`${url}/api/notes` + noteId, note);
+    const response = await axios.patch(`${url}/api/notes/` + noteId, note);
     return response.data;
 };
