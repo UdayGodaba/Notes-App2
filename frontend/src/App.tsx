ea928f17f1e72aa371581008b7c3d19c7d376dec
UdayGodaba <godabauday@gmail.com>
2023-06-25 11:51:04 +0530

    alert added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import { Alert } from 'react-bootstrap';
 import Header from './components/Header';
 import NotesView from './components/NotesView';
+import { useState } from 'react';
 
 function App() {
 
+  const [view, setView] = useState(true);
+  setTimeout(() => {
+    setView(false);
+  }, 2000);
+
   return (
     <>
-      setTimeout(function () {
+      {view &&
         <Alert>Please do refresh few times if notes did not appear as backend is hosted on a free hosting service it is not up always</Alert>
-      }, 2000);
+      }
       <Header />
       <NotesView />
     </>
